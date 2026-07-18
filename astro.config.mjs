996613f0c16d49e56a0daa2397712543ea3679e4
1azarito <192937226+lazzzarito@users.noreply.github.com>
2026-07-18 18:11:00 -0400

    Update 26.8 (New Design)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import icon from 'astro-icon';
+
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://lazzzarito.github.io/Portfolio',
-  outDir: 'dist',
-  publicDir: 'public',
-  build: {
-    assets: '_assets',
+  // site: '#',
+  // base: '/',
+  compressHTML: true,
+  vite: {
+    plugins: [tailwindcss()]
   },
+
+  integrations: [icon()]
 });
